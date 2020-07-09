7feb5e61962ea45d9e74088353f7fe525c96ecee
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-07-08 20:31:08 -0500

    finished mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,15 +7,17 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    arr.sort()
     start = 0
     end = (len(arr) - 1)
     found = False
 
-    while end <= start and not found:
+    while end >= start and not found:
         middle_index = (start + end) // 2
 
         if arr[middle_index] == target:
             found = True
+            return middle_index
         else:
             if target < arr[middle_index]:
                 end = middle_index - 1
